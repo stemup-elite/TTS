7428c9aa104698023207fb9bc5f8376c36d30f9d
vutuandat20385 <vutuandat.20385@gmail.com>
2019-10-03 10:46:59 +0700

    source code reup

---
+++ application/views/sadmin/head.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+ <!DOCTYPE html>
+ <html>
+ <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Dashboard</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="<?php echo base_url('sadmin/bower_components/bootstrap/dist/css/bootstrap.min.css')?>">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo base_url('sadmin/bower_components/font-awesome/css/font-awesome.min.css')?>">
+
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="<?php echo base_url('sadmin/bower_components/Ionicons/css/ionicons.min.css')?>">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url('sadmin/dist/css/AdminLTE.min.css')?>">
+  
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+   folder instead of downloading all of them to reduce the load. -->
+   <link rel="stylesheet" href="<?php echo base_url('sadmin/dist/css/skins/_all-skins.min.css')?>">
+   <!-- jvectormap -->
+   <link rel="stylesheet" href="<?php echo base_url('sadmin/bower_components/jvectormap/jquery-jvectormap.css')?>">
+   <!-- Date Picker -->
+   <link rel="stylesheet" href="<?php echo base_url('sadmin/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')?>">
+   <!-- Daterange picker -->
+   <link rel="stylesheet" href="<?php echo base_url('sadmin/bower_components/bootstrap-daterangepicker/daterangepicker.css')?>">
+   <!-- bootstrap wysihtml5 - text editor -->
+   <link rel="stylesheet" href="<?php echo base_url('sadmin/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')?>">
+<script type="text/javascript" src="<?php echo base_url();?>editor/tinymce.min.js"></script>
+   <link rel="stylesheet" href="<?php echo base_url('css/style-sadmin.css')?>">
+   <!-- Google Font -->
+   <script src="<?php echo base_url('ckeditor/ckeditor.js') ?>" charset="utf-8"></script>
+<script src="<?php echo base_url('ckfinder/ckfinder.js') ?>"></script>
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+   <script src="<?php echo base_url('js/create_quiz_stemup.js') ?>"></script>
+   <script src="<?php echo base_url('js/new_stemup.js')  ?>"></script>
+<!-- <script src="<?php //echo base_url('js/send_email.js')  ?>"></script> -->
+
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
